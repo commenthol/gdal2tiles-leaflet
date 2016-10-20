56b04567cf0337910a5706245272c1491bdf5f93
commenthol <commenthol@gmail.com>
2016-10-20 23:16:37 +0200

    test

--- test/createtiles.sh
+++ test/createtiles.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-../gdal2tiles.py -l -p raster -z 0-5 -w none karta.jpg tiles
+rm -rf tiles
+
+case $1 in
+  mpz)
+    ../gdal2tiles-multiprocess.py -l -p raster -z 0-5 -w none karta.jpg tiles
+    ;;
+  mp)
+    ../gdal2tiles-multiprocess.py -l -p raster -w none karta.jpg tiles
+    ;;
+  z)
+    ../gdal2tiles.py -l -p raster -w none karta.jpg -z 0-5 tiles
+    ;;
+  *)
+    ../gdal2tiles.py -l -p raster -w none karta.jpg tiles
+    ;;
+esac
