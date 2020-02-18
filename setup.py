670d714e0ba57768cb3f1e3f16d4e78922027862
commenthol <commenthol@gmail.com>
2020-02-18 22:00:09 +0100

    feat: add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='gdal2tiles_leaflet',
+    version='0.1.0',
+    description='Generate raster image tiles for use with leaflet',
+    url='https://github.com/commenthol/gdal2tiles-leaflet',
+    license='MIT',
+    install_requires=[
+      'gdal',
+    ],
+)
